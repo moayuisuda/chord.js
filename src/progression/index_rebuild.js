c48be3968caec2d0e6df0976614036f39526d593
Your Name <you@example.com>
2019-11-15 19:45:17 +0800

    an

--- src/progression/index_rebuild.js
+++ src/progression/index_rebuild.js
@@ -4,9 +4,17 @@ import {
     Loop,
     Ticks,
 } from 'tone'
-import {template, loopMap, synth} from './sources'
-import {chord as getChord} from '../rad.js/index'
-import {ChordItem} from './chordItem'
+import {
+    template,
+    loopMap,
+    synth
+} from './sources'
+import {
+    chord as getChord
+} from '../rad.js/index'
+import {
+    ChordItem
+} from './chordItem'
 import Vue from 'vue/dist/vue.js'
 
 let instance = new Vue({
@@ -31,9 +39,19 @@ let instance = new Vue({
     watch: {
         bpm: {
             handler(val, oldVal) {
-            Transport.bpm.value = val;
+                Transport.bpm.value = val;
             },
             immediate: true
+        },
+        'timeline.length': {
+            handler(val) {
+                console.log(val);
+                let tile = this.timeline[this.timeline.length - 1];
+                if (tile) {
+                    Transport.loopEnd = this.timeline[this.timeline.length - 1].stop;
+                    Transport.loop = true;
+                } else Transport.loopEnd = false;
+            },
         }
     },
 
@@ -42,60 +60,64 @@ let instance = new Vue({
             if (this.playing) this.stop();
             else this.start();
         },
-    
+
         start() {
             this.playing = true;
             Transport.stop();
             this.caculateTime();
             Transport.start();
         },
-    
+
         stop() {
             this.playing = false;
             this.flag = this.timeline.length - 1;
             Transport.stop();
         },
-    
+
         // single is one beat length, and amount is how many times this beat would be triggered.
         add() {
-            let {amount, single, chord} = this.input;
+            let {
+                amount,
+                single,
+                chord
+            } = this.input;
             let chordArr
             try {
-             chordArr = getChord(chord, this.initOctive);
-            } catch(e) {
+                chordArr = getChord(chord, this.initOctive);
+            } catch (e) {
                 console.log(e);
                 return;
             }
-    
+
             if (!(amount.match(/^\d$/) && single.match(/^\d$/)))
                 throw `[Rad-Club] The parameter "${amount}/${single}" is not valid`
-    
+
             let time = Time(`${single}n`) * amount,
-            loop = new Loop((time) => {
-                loopMap[this.type](synth, chordArr, time, single);
-            }, `${single}n`),
-            chordItem = new ChordItem({
-                chord,
-                loop,
-                time,
-                amount,
-                single,
-                instance: this
-            })
+                loop = new Loop((time) => {
+                    loopMap[this.type](synth, chordArr, time, single);
+                }, `${single}n`),
+                chordItem = new ChordItem({
+                    chord,
+                    loop,
+                    time,
+                    amount,
+                    single,
+                    instance: this
+                })
 
             console.log(chordItem.amount, chordItem.single)
-    
+
             this.timeline.splice(this.flag + 1, 0, chordItem);
             this.caculateTime();
 
             this.timeline[chordItem.flag].focus();
         },
-    
+
         remove(instance) {
             this.timeline.splice(instance.flag, 1);
             this.caculateTime();
         },
-    
+
         caculateTime() {
             let timeFlag = 0;
             let flag = 0;
@@ -104,19 +126,19 @@ let instance = new Vue({
                     time,
                     loop
                 } = item;
-    
+
                 let start = timeFlag;
                 timeFlag += time;
                 let stop = timeFlag;
 
                 item.start = start;
                 item.stop = stop;
-                item.flag = flag ++;
+                item.flag = flag++;
 
                 loop.cancel();
                 loop.start(start);
                 loop.stop(stop);
-    
+
                 Transport.schedule((time) => {
                     item.setFlag();
                 }, start);
@@ -129,4 +151,6 @@ let instance = new Vue({
     }
 })
 
-export {instance}
+export {
+    instance
+}
