fb783693dcd025d9726e92c9a04f34df247e5747
v991314 <959398812@163.com>
2019-11-30 19:50:13 +0800

    an

--- test.js
+++ test.js
@@ -1,18 +1,1 @@
-var isValid = function(s) {
-    let stack = s.split('').reverse();
-
-    for(let i = stack.length - 1; i >= 0; i --) {
-        let index;
-        let item = stack[i];
-        if(stack.lastIndexOf(item) != -1) {
-            index = stack.lastIndexOf(item);
-            stack.pop();
-            stack.splice(index, 1);
-        } 
-    }
-
-    console.log(stack)
-    return stack.length == 0;
-};
-
-console.log(isValid('(]'))// 10
+console.log(" ".length)
